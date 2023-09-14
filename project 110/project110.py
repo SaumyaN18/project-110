194cbce69bc8fcc8d5e801827d5d583fefb36a24
Saumya Narang <101644310+SaumyaN18@users.noreply.github.com>
2023-09-14 20:33:38 +0530

    Add files via upload

---
+++ project 110/project110.py
@@ -0,0 +1,25 @@
+import random
+response='y'
+while response=='y':
+    no=random.randint(1,6)
+    if no==1:
+        print('[0]')
+    if no ==2:
+        print('[0  0]')
+    if no ==3:
+        print('''[0
+                    0
+                      0]''')
+    if no==4:
+        print('''[0   0 
+                  0   0]''')
+    if no==5:
+        print('''[0   0 
+                    0
+                  0   0]''')
+    if no == 6:
+        print('''[0   0 
+                  0   0
+                  0   0]''')
+    response = input('Press y to roll again and pres n to leave')
+    print('\n')
